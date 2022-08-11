43399dd671da8156244f4583d8fcd6a911e9b00d
Taka <yukioinokinawa@gmail.com>
2022-08-11 13:56:28 +0900

    front_page dane? day2

--- js/menu.js
+++ js/menu.js
@@ -1,6 +1,7 @@
 jQuery(function ($) {
     $(".c-button").on("click", function () {
         $(this).toggleClass("is-open");
+        $("#shadow").toggleClass("go");
         $(".l-nav").toggleClass("is-open");
     });
 });
