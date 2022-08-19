0fbd58c1a77dc05af1c8a360a63de8773c8a9b18
Taka <yukioinokinawa@gmail.com>
2022-08-19 18:38:12 +0900

    static site done?

--- js/menu.js
+++ js/menu.js
@@ -1,5 +1,5 @@
 jQuery(function ($) {
-    $(".c-button").on("click", function () {
+    $(".c-menubutton").on("click", function () {
         $(this).toggleClass("is-open");
         $("#shadow").toggleClass("go");
         $(".l-nav").toggleClass("is-open");
